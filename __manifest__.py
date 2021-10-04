377fd037dba40d8f00ff594b20e511999a8d29d0
omprakashkyadav <omprakashopj90@gmail.com>
2021-10-04 21:27:51 +0530

    [ADDED] min require file to install odoo module

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# _*_ Coding: UTF-8 _*_
+
+{
+    "name": "Hospital Management",
+    "summery": "*** This Module for managing the hospital ***",
+    "version": "12.0.0.0",
+    "category": "Extra Tools",
+    "license": "LGPL-3",
+    "author": "Omprakash Yadav",
+    "website": "https://github.com/omprakashkyadav/",
+    "depends": ['base'],
+    "data": [
+
+    ],
+    "images": [],
+    "application": False,
+    "installable": True,
+    "auto_install": False
+}
