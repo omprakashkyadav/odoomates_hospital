b039d011e7c01e1aef36f742a5dfc3fd891f0722
omprakashkyadav <omprakashopj90@gmail.com>
2021-11-22 16:50:36 +0530

    [ADDED] Create Excel xls report in odoo

---
+++ reports/__init__.py
@@ -0,0 +1,3 @@
+# _*_ Coding: utf-8 _*_
+
+from . import report_card_xls
---
+++ reports/report_card_xls.py
@@ -0,0 +1,20 @@
+# _*_ Coding: utf-8 _*_
+
+from odoo import models, fields
+
+
+class PartnerXlsx(models.AbstractModel):
+    _name = 'report.odoomates_hospital.report_patient_xls'
+    _inherit = 'report.report_xlsx.abstract'
+
+    def generate_xlsx_report(self, workbook, data, lines):
+        format1 = workbook.add_format({'font_size': 14, 'align': 'vcenter', 'bold': True})
+        format2 = workbook.add_format({'font_size': 10, 'align': 'vcenter', })
+        sheet = workbook.add_worksheet('Patient Card')
+        sheet.write(2, 2, 'Name', format1)
+        sheet.write(2, 3, lines.patient_name, format2)
+        sheet.write(3, 2, 'Age', format1)
+        sheet.write(3, 3, lines.patient_age, format2)
+
+
+
